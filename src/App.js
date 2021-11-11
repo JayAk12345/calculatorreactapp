8c387e3556780ab4dbe66b0a1abb16767c94c169
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-11 15:11:22 +0000

    adds components to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Numbers from "./components/numbers";
+import Operations from "./components/operations";
+import Display from "./components/display";
+import { useState } from "react";
 
 function App() {
+  const [display, setDisplay] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <Display display={display} />
+      <Operations />
+      <Numbers display={display} setDisplay={setDisplay} />
     </div>
   );
 }
