9ef4fd91a691fe05b574c1b340d93f16945d4590
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-12 12:00:53 +0000

    adds functionality for decimal numbers

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -1,57 +1,55 @@
-const Calculate = ({display, setDisplay}) => {
-    return (
-        <div>
-            <button onClick={() => {
-                const separatedComps = splitDisplay(display);
-                setDisplay(calc(separatedComps));
-
-            }
-            }  
-            > 
-            =</button>
-        </div>
-    )
-}
+const Calculate = ({ display, setDisplay }) => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          const separatedComps = splitDisplay(display);
+          setDisplay(calc(separatedComps));
+        }}
+      >
+        =
+      </button>
+    </div>
+  );
+};
 
 export default Calculate;
 
-
 const splitDisplay = (display) => {
-    const firstNum = [];
-    let ops = '';
+  const firstNum = [];
+  let ops = "";
 
-    for (let i=0; i<display.length; i++) {
-        if(typeof display[i] === 'number') {
-            firstNum.push(display[i])
-        } else {
-            ops=display[i]
-            break;
-        }
+  for (let i = 0; i < display.length; i++) {
+    if (typeof display[i] === "number") {
+      firstNum.push(display[i]);
+    } else if (display[i] === ".") {
+      firstNum.push(display[i]);
+    } else {
+      ops = display[i];
+      break;
     }
+  }
 
-    let secondNum = display.slice(display.indexOf(ops)+1);
-
-    const parsedFirstNum = parseInt(firstNum.join(''));
-    const parsedSecondNum = parseInt(secondNum.join(''));
-    let seperateComps = [parsedFirstNum, ops, parsedSecondNum];
-    return seperateComps;
-}
+  let secondNum = display.slice(display.indexOf(ops) + 1);
+  const parsedFirstNum = parseFloat(firstNum.join(""));
+  const parsedSecondNum = parseFloat(secondNum.join(""));
+  let seperateComps = [parsedFirstNum, ops, parsedSecondNum];
+  return seperateComps;
+};
 
 const calc = ([parsedFirstNum, ops, parsedSecondNum]) => {
-    let answer = 0;
-    if(ops === '+') {
-        answer += parsedFirstNum + parsedSecondNum;
-    }
-    if(ops === '-') {
-        answer += parsedFirstNum - parsedSecondNum;
-    }
-    if(ops === '/') {
-        answer += parsedFirstNum / parsedSecondNum;
-    }
-    if(ops === 'X') {
-        answer += parsedFirstNum * parsedSecondNum;
-    }
-    console.log(answer);
-    return answer;
-}
-
+  let answer = 0;
+  if (ops === "+") {
+    answer += parsedFirstNum + parsedSecondNum;
+  }
+  if (ops === "-") {
+    answer += parsedFirstNum - parsedSecondNum;
+  }
+  if (ops === "/") {
+    answer += parsedFirstNum / parsedSecondNum;
+  }
+  if (ops === "X") {
+    answer += parsedFirstNum * parsedSecondNum;
+  }
+  return answer;
+};
