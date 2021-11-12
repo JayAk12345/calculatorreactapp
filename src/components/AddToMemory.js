a813d6136fb745e970239ad77a29cfa5e8e03146
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-12 15:49:55 +0000

    adds memory button componenets

--- src/components/AddToMemory.js
+++ src/components/AddToMemory.js
@@ -1,27 +1,17 @@
-import { useState } from "react";
-
-const AddToMemory = ({ display, memoryBlock }) => {
-  let [memory, setMemory] = useState({
-    m1: [],
-    m2: [],
-    m3: [],
-    m4: []
-  })
-
+const AddToMemory = ({ display, memoryBlock, memory, setMemory }) => {
   return (
     <div>
       <button
         onClick={() => {
           setMemory((currMemory) => {
-            let memoryCopy = {...currMemory};
+            let memoryCopy = { ...currMemory };
             memoryCopy[memoryBlock] = display;
             return memoryCopy;
-          })
+          });
         }}
       >
         {memoryBlock}:
       </button>
-      {memory[memoryBlock]}
     </div>
   );
 };
--- src/components/memoryButtons.js
+++ src/components/memoryButtons.js
@@ -1,29 +1,76 @@
 import AddToDisplay from "./addToDisplay";
 import AddToMemory from "./AddToMemory";
+import { useState } from "react";
 
 const MemoryButtons = ({ display, setDisplay }) => {
+  let [memory, setMemory] = useState({
+    m1: [],
+    m2: [],
+    m3: [],
+    m4: [],
+  });
+
   return (
     <div className="memory">
       <table id="memoryTable">
         <tr>
           <td>
-            <AddToMemory display={display} memoryBlock={'M1'} />
-            <AddToDisplay display={display} setDisplay={setDisplay} num={5} />
+            <AddToMemory
+              display={display}
+              memoryBlock={"m1"}
+              memory={memory}
+              setMemory={setMemory}
+            />
+            <AddToDisplay
+              display={display}
+              setDisplay={setDisplay}
+              num={memory.m1}
+            />
           </td>
         </tr>
         <tr>
           <td>
-            <AddToMemory display={display} memoryBlock={'M2'}/>
+            <AddToMemory
+              display={display}
+              memoryBlock={"m1"}
+              memory={memory}
+              setMemory={setMemory}
+            />
+            <AddToDisplay
+              display={display}
+              setDisplay={setDisplay}
+              num={memory.m2}
+            />
           </td>
         </tr>
         <tr>
           <td>
-            <AddToMemory display={display} memoryBlock={'M3'}/>
+            <AddToMemory
+              display={display}
+              memoryBlock={"m1"}
+              memory={memory}
+              setMemory={setMemory}
+            />
+            <AddToDisplay
+              display={display}
+              setDisplay={setDisplay}
+              num={memory.m1}
+            />
           </td>
         </tr>
         <tr>
           <td>
-            <AddToMemory display={display} memoryBlock={'M4'}/>
+            <AddToMemory
+              display={display}
+              memoryBlock={"m1"}
+              memory={memory}
+              setMemory={setMemory}
+            />
+            <AddToDisplay
+              display={display}
+              setDisplay={setDisplay}
+              num={memory.m1}
+            />
           </td>
         </tr>
       </table>
